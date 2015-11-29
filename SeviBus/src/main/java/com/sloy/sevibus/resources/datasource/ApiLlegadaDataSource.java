5bc3bf61f58f0f43d1d77f39677ed440f62ebf28
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-11-29 21:17:48 +0100

    Añade timeout de 5 segundos a las peticiones a API
    Para que no se queje demasiada gente

--- SeviBus/src/main/java/com/sloy/sevibus/resources/datasource/ApiLlegadaDataSource.java
+++ SeviBus/src/main/java/com/sloy/sevibus/resources/datasource/ApiLlegadaDataSource.java
@@ -4,6 +4,7 @@ import com.sloy.sevibus.model.ArrivalTime;
 import com.sloy.sevibus.resources.exceptions.ServerErrorException;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 
@@ -21,6 +22,7 @@ public class ApiLlegadaDataSource implements LlegadaDataSource {
     public Observable<ArrivalTime> getLlegadas(Integer parada, List<String> lineas) throws ServerErrorException {
         return sevibusApi.getArrivals(parada, lineas)
           .retry(1)
+          .timeout(5, TimeUnit.SECONDS)
           .flatMap(Observable::from)
           .onErrorResumeNext(e -> fallbackDataSource.getLlegadas(parada, lineas));
     }
