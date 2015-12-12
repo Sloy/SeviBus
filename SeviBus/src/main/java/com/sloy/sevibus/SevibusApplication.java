954966d155fec5577013d17292609d80dca5ca40
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-12-12 14:10:28 +0100

    Soporte para Instant Run
    Multidex
    Minsk 21
    Gradle plugin 2.0.0
    Update tools

--- SeviBus/src/main/java/com/sloy/sevibus/SevibusApplication.java
+++ SeviBus/src/main/java/com/sloy/sevibus/SevibusApplication.java
@@ -1,16 +1,14 @@
 package com.sloy.sevibus;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
 import com.facebook.stetho.Stetho;
-import com.sloy.sevibus.resources.Debug;
-import com.sloy.sevibus.resources.StuffProvider;
 
 import io.fabric.sdk.android.Fabric;
 
-public class SevibusApplication extends Application{
+public class SevibusApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
