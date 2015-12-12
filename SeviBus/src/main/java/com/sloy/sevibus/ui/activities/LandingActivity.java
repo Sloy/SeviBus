c35bf18bf3fe1bf72410e6bf174623f99df6d89e
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-12-12 13:40:57 +0100

    Creada nueva activity de landing
    Sólo el estilo por ahora
    
    Cambia subtítulo del permiso
    
    Botones de permisos clicables y más pequeños
    
    Texto de permiso centrado
    
    Moar padding
    
    Pantalla de permiso con un gran icono y botones
    
    Diseño responsive una mijita
    
    WIP

---
+++ SeviBus/src/main/java/com/sloy/sevibus/ui/activities/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.sloy.sevibus.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sloy.sevibus.R;
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
