cefc9bd33e64dc5c2dc9d84f01e296e86d73a7ad
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-01-28 00:53:11 +0100

    Integrated Firebase into the proyect

--- SeviBus/src/main/java/com/sloy/sevibus/SevibusApplication.java
+++ SeviBus/src/main/java/com/sloy/sevibus/SevibusApplication.java
@@ -5,6 +5,7 @@ import android.support.multidex.MultiDexApplication;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
 import com.facebook.stetho.Stetho;
+import com.firebase.client.Firebase;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -14,6 +15,8 @@ public class SevibusApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics(), new Answers());
+        Firebase.setAndroidContext(this);
+
 
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this);
