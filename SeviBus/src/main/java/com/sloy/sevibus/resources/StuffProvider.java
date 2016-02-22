db81d0e822a0b14740fbcb9b6f565d7076d0b835
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-02-22 20:46:20 +0100

    Configured StuffProvider for all the new dependencies

--- SeviBus/src/main/java/com/sloy/sevibus/resources/StuffProvider.java
+++ SeviBus/src/main/java/com/sloy/sevibus/resources/StuffProvider.java
@@ -1,9 +1,12 @@
 package com.sloy.sevibus.resources;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.crashlytics.android.answers.Answers;
 import com.firebase.client.Firebase;
+import com.google.gson.Gson;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.sloy.sevibus.BuildConfig;
 import com.sloy.sevibus.bbdd.DBHelper;
@@ -11,20 +14,26 @@ import com.sloy.sevibus.resources.actions.ObtainCercanasAction;
 import com.sloy.sevibus.resources.actions.ObtainLineasCercanasAction;
 import com.sloy.sevibus.resources.actions.favorita.DeleteFavoritaAction;
 import com.sloy.sevibus.resources.actions.favorita.ObtainFavoritasAction;
-import com.sloy.sevibus.resources.actions.llegada.ObtainLlegadasAction;
 import com.sloy.sevibus.resources.actions.favorita.ObtainSingleFavoritaAction;
 import com.sloy.sevibus.resources.actions.favorita.ReorderFavoritasAction;
 import com.sloy.sevibus.resources.actions.favorita.SaveFavoritaAction;
+import com.sloy.sevibus.resources.actions.llegada.ObtainLlegadasAction;
+import com.sloy.sevibus.resources.actions.user.LogInAction;
+import com.sloy.sevibus.resources.actions.user.LogOutAction;
+import com.sloy.sevibus.resources.actions.user.ObtainUserAction;
 import com.sloy.sevibus.resources.datasource.ApiErrorHandler;
 import com.sloy.sevibus.resources.datasource.LineaDataSource;
 import com.sloy.sevibus.resources.datasource.llegada.ApiLlegadaDataSource;
+import com.sloy.sevibus.resources.datasource.StringDownloader;
 import com.sloy.sevibus.resources.datasource.favorita.DBFavoritaDataSource;
 import com.sloy.sevibus.resources.datasource.favorita.FavoritaDataSource;
 import com.sloy.sevibus.resources.datasource.favorita.FirebaseFavoritaDataSource;
+import com.sloy.sevibus.resources.datasource.llegada.ApiLlegadaDataSource;
 import com.sloy.sevibus.resources.datasource.llegada.LlegadaDataSource;
 import com.sloy.sevibus.resources.datasource.llegada.SevibusApi;
-import com.sloy.sevibus.resources.datasource.StringDownloader;
 import com.sloy.sevibus.resources.datasource.llegada.TussamLlegadaDataSource;
+import com.sloy.sevibus.resources.datasource.user.PreferencesUserDataSource;
+import com.sloy.sevibus.resources.datasource.user.UserDataSource;
 import com.sloy.sevibus.resources.sync.UpdateDatabaseAction;
 
 import retrofit.RestAdapter;
@@ -36,6 +45,14 @@ public class StuffProvider {
     public static final String API_ENDPOINT = PRODUCTION_API_ENDPOINT;
 
     private static CrashReportingTool crashReportingToolInstance;
+
+    public static Gson getGson() {
+        return new Gson();
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
 
     public static UpdateDatabaseAction getUpdateDatabaseAction(Context context) {
         return new UpdateDatabaseAction(context, getDbHelper(context), getStringDownloader());
@@ -90,7 +107,7 @@ public class StuffProvider {
     }
 
     public static ObtainFavoritasAction getObtainFavoritasAction(Context context) {
-        return new ObtainFavoritasAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource());
+        return new ObtainFavoritasAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource(context));
     }
 
     public static ObtainSingleFavoritaAction getObtainSingleFavoritaAction(Context context) {
@@ -98,22 +115,22 @@ public class StuffProvider {
     }
 
     public static DeleteFavoritaAction getDeleteFavoritaAction(Context context) {
-        return new DeleteFavoritaAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource());
+        return new DeleteFavoritaAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource(context));
     }
 
     public static SaveFavoritaAction getSaveFavoritaAction(Context context) {
-        return new SaveFavoritaAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource(), getDbHelper(context));
+        return new SaveFavoritaAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource(context), getDbHelper(context));
     }
-
     public static ReorderFavoritasAction getReorderFavoritasAction(Context context) {
-        return new ReorderFavoritasAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource());
+        return new ReorderFavoritasAction(getLocalFavoritaDataSource(context), getRemoteFavoritaDataSource(context));
     }
 
     public static FavoritaDataSource getLocalFavoritaDataSource(Context context) {
         return new DBFavoritaDataSource(getDbHelper(context));
     }
-    public static FavoritaDataSource getRemoteFavoritaDataSource() {
-        return new FirebaseFavoritaDataSource(getFirebase());
+
+    public static FavoritaDataSource getRemoteFavoritaDataSource(Context context) {
+        return new FirebaseFavoritaDataSource(getFirebase(), getUserDataSource(context));
     }
 
     public static Firebase getFirebase() {
@@ -125,6 +142,22 @@ public class StuffProvider {
     }
 
     public static ObtainLineasCercanasAction getObtainLineasCercanasAction(Context context) {
-        return new ObtainLineasCercanasAction(new LineaDataSource(getDbHelper(context)),getObtainCercanasAction(context));
+        return new ObtainLineasCercanasAction(new LineaDataSource(getDbHelper(context)), getObtainCercanasAction(context));
+    }
+
+    public static ObtainUserAction getObtainUserAction(Context context) {
+        return new ObtainUserAction(getUserDataSource(context));
+    }
+
+    private static UserDataSource getUserDataSource(Context context) {
+        return new PreferencesUserDataSource(getSharedPreferences(context), getGson());
+    }
+
+    public static LogInAction getLoginAction(Context context) {
+        return new LogInAction(getUserDataSource(context));
+    }
+
+    public static LogOutAction getLogoutAction(Context context) {
+        return new LogOutAction(getUserDataSource(context));
     }
 }
