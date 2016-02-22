aa66a3bc53f42045688f24974b0b19c146da25f3
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-02-22 20:34:31 +0100

    Simplified ObtainUserAction
    Yeah... It was a nice TTD-made product, but it was doing more things that what I need right now.

--- SeviBus/src/test/java/com/sloy/sevibus/resources/actions/user/ObtainUserActionTest.java
+++ SeviBus/src/test/java/com/sloy/sevibus/resources/actions/user/ObtainUserActionTest.java
@@ -1,7 +1,6 @@
 package com.sloy.sevibus.resources.actions.user;
 
 import com.google.common.base.Optional;
-import com.sloy.sevibus.resources.Session;
 import com.sloy.sevibus.resources.datasource.user.UserDataSource;
 import com.sloy.sevibus.ui.SevibusUser;
 
@@ -13,7 +12,6 @@ import org.mockito.MockitoAnnotations;
 import rx.Observable;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ObtainUserActionTest {
@@ -22,30 +20,17 @@ public class ObtainUserActionTest {
 
     @Mock
     UserDataSource userDataSource;
-    @Mock
-    Session session;
 
     private ObtainUserAction obtainCurrentUser;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        obtainCurrentUser = new ObtainUserAction(userDataSource, session);
+        obtainCurrentUser = new ObtainUserAction(userDataSource);
     }
 
     @Test
-    public void returns_user_in_session_when_user_present() throws Exception {
-        when(session.getUser()).thenReturn(Optional.of(STUB_USER));
-
-        Optional<SevibusUser> result = obtainCurrentUser.obtainUser().toBlocking().single();
-
-        assertThat(result.isPresent()).isTrue();
-        assertThat(result.get()).isEqualTo(STUB_USER);
-    }
-
-    @Test
-    public void returns_user_in_datasource_when_user_not_in_session() throws Exception {
-        when(session.getUser()).thenReturn(Optional.absent());
+    public void returns_user_in_datasource_when_present() throws Exception {
         when(userDataSource.getCurrentUser()).thenReturn(Observable.just(STUB_USER));
 
         Optional<SevibusUser> result = obtainCurrentUser.obtainUser().toBlocking().single();
@@ -55,23 +40,11 @@ public class ObtainUserActionTest {
     }
 
     @Test
-    public void returns_absent_when_user_not_in_session_or_datasource() throws Exception {
-        when(session.getUser()).thenReturn(Optional.absent());
+    public void returns_absent_when_user_not_in_datasource() throws Exception {
         when(userDataSource.getCurrentUser()).thenReturn(Observable.empty());
 
         Optional<SevibusUser> result = obtainCurrentUser.obtainUser().toBlocking().single();
 
         assertThat(result.isPresent()).isFalse();
     }
-
-    @Test
-    public void stores_user_in_session_when_user_is_absent_in_session_and_exist_in_datasource() throws Exception {
-        when(session.getUser()).thenReturn(Optional.absent());
-        when(userDataSource.getCurrentUser()).thenReturn(Observable.just(STUB_USER));
-
-        obtainCurrentUser.obtainUser().toBlocking().single();
-
-        verify(session).setUser(STUB_USER);
-    }
-
 }
