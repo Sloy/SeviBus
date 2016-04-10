bdf2e0acebdd54c816a828d815de7fdac367e339
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-04-10 21:32:32 +0200

    Removed changelog from main screen
    It's not working well, and probably nobody cares about it.

--- SeviBus/src/main/java/com/sloy/sevibus/ui/fragments/MainPageFragment.java
+++ SeviBus/src/main/java/com/sloy/sevibus/ui/fragments/MainPageFragment.java
@@ -1,14 +1,8 @@
 package com.sloy.sevibus.ui.fragments;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,13 +10,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sloy.sevibus.BuildConfig;
 import com.sloy.sevibus.R;
 import com.sloy.sevibus.resources.LocationProvider;
 import com.sloy.sevibus.resources.StuffProvider;
 import com.sloy.sevibus.ui.activities.BusquedaActivity;
 import com.sloy.sevibus.ui.activities.LocationProviderActivity;
-import com.sloy.sevibus.ui.activities.PreferenciasActivity;
 import com.sloy.sevibus.ui.mvp.presenter.FavoritasMainPresenter;
 import com.sloy.sevibus.ui.mvp.presenter.LineasCercanasPresenter;
 import com.sloy.sevibus.ui.mvp.presenter.ParadasCercanasMainPresenter;
@@ -30,15 +22,7 @@ import com.sloy.sevibus.ui.mvp.view.FavoritasMainViewContainer;
 import com.sloy.sevibus.ui.mvp.view.LineasCercanasViewContainer;
 import com.sloy.sevibus.ui.mvp.view.ParadasCercanasMainViewContainer;
 
-import de.cketti.library.changelog.ChangeLog;
-
 public class MainPageFragment extends BaseDBFragment {
-
-    private static final String FRAG_PARADAS_CERCANAS = "f_p_cercanas";
-    private static final String FRAG_LINEAS_CERCANAS = "f_l_cercanas";
-
-    private static final String PREF_SHOW_NEW_VERSION_LATEST_SEEN = "newversion_last_seen";
-    private static final int NEW_VERSION_SNACKBAR_DURATION = 10000;
 
     private FavoritasMainPresenter favoritasPresenter;
     private ParadasCercanasMainPresenter cercanasPresenter;
@@ -93,42 +77,6 @@ public class MainPageFragment extends BaseDBFragment {
         favoritasPresenter.pause();
         cercanasPresenter.pause();
         lineasCercanasPresenter.pause();
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setupUI();
-    }
-
-    private void setupUI() {
-        setupNewVersion();
-    }
-
-    private void setupNewVersion() {
-        final SharedPreferences prefs = getActivity().getSharedPreferences(PreferenciasActivity.PREFS_CONFIG_VALUES, Context.MODE_PRIVATE);
-        int lastVersion = prefs.getInt(PREF_SHOW_NEW_VERSION_LATEST_SEEN, 0);
-        int currentVersion = BuildConfig.VERSION_CODE;
-
-        if (lastVersion < currentVersion) {
-            ChangeLog cl = new ChangeLog(getActivity());
-            boolean hasChangelog = !cl.getChangeLog(false).isEmpty();
-            if (hasChangelog) {
-                new Handler().postDelayed(() -> {
-                    if (!isAdded() || getView() == null) {
-                        return;
-                    }
-                    //noinspection ResourceType
-                    Snackbar.make(getView(), "SeviBus se ha actualizado!", NEW_VERSION_SNACKBAR_DURATION)
-                      .setAction("Ver cambios", v -> {
-                          cl.getFullLogDialog().show();
-                      })
-                      .show();
-                }, 1000);
-            }
-        }
-
-        prefs.edit().putInt(PREF_SHOW_NEW_VERSION_LATEST_SEEN, currentVersion).apply();
     }
 
     @Override
