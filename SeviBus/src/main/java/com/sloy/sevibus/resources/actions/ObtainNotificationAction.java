33239ffd7efb5745ed12325ad1877aa618163615
Sergio Rodríguez Calvo <serrodcal@MacBook-Pro-de-Sergio.local>
2016-05-29 13:58:52 +0200

    Create ObtainNotificationAction

---
+++ SeviBus/src/main/java/com/sloy/sevibus/resources/actions/ObtainNotificationAction.java
@@ -0,0 +1,16 @@
+package com.sloy.sevibus.resources.actions;
+
+import com.sloydev.gallego.Optional;
+
+import rx.Observable;
+
+/**
+ * Created by serrodcal on 29/5/16.
+ */
+public class ObtainNotificationAction {
+
+    public Observable<Optional<String>> getNotification() {
+        return null;
+    }
+
+}
